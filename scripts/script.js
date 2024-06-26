bdfa2df875f67d2c43d1966a0d2f00dd99bdacfa
Lynx1032 <hohoangnghiep.2002@gmail.com>
2024-06-27 00:55:37 +0700

    init

---
+++ scripts/script.js
@@ -0,0 +1,15 @@
+function updateTime() {
+    var currentTime = new Date();
+    var hours = currentTime.getHours();
+    var minutes = currentTime.getMinutes();
+    var seconds = currentTime.getSeconds();
+
+    hours = (hours < 10 ? "0" : "") + hours;
+    minutes = (minutes < 10 ? "0" : "") + minutes;
+    seconds = (seconds < 10 ? "0" : "") + seconds;
+
+    var timeString = hours + ":" + minutes + ":" + seconds;
+    document.getElementById("time").innerHTML = timeString;
+}
+
+setInterval(updateTime, 1000);
