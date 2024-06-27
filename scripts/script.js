44f07c97744248e68acf140e4dd671c3130613bd
Lynx1032 <hohoangnghiep.2002@gmail.com>
2024-06-28 01:06:33 +0700

    2862024-106

--- scripts/script.js
+++ scripts/script.js
@@ -25,18 +25,88 @@ function updateDate() {
   document.getElementById("date").innerHTML = dateString;
 }
 
+function adjustTextSize() {
+  var viewportWidth = window.innerWidth || document.documentElement.clientWidth;
+  var viewportHeight = window.innerHeight || document.documentElement.clientHeight;
+
+  if ((viewportHeight <= 2160) && (viewportHeight > 1440) && (viewportHeight < viewportWidth)) {
+    var textTimeElement = document.getElementById("time");
+    textTimeElement.style.fontSize = "6vw";
+    textTimeElement.style.textAlign = "right";
+
+    var textDateElement = document.getElementById("date");
+    textDateElement.style.fontSize = "4vw";
+    textDateElement.style.textAlign = "right";
+  } else 
+  
+  if ((viewportHeight <= 1440) && (viewportHeight > 1080) && (viewportHeight < viewportWidth)) {
+    var textTimeElement = document.getElementById("time");
+    textTimeElement.style.fontSize = "5vw";
+    textTimeElement.style.textAlign = "right";
+
+    var textDateElement = document.getElementById("date");
+    textDateElement.style.fontSize = "3vw";
+    textDateElement.style.textAlign = "right";
+  } else
+
+  if ((viewportHeight <= 1080) && (viewportHeight < viewportWidth)) {
+    var textTimeElement = document.getElementById("time");
+    textTimeElement.style.fontSize = "4vw";
+    textTimeElement.style.textAlign = "right";
+
+    var textDateElement = document.getElementById("date");
+    textDateElement.style.fontSize = "2vw";
+    textDateElement.style.textAlign = "right";
+  } else 
+
+  if (viewportHeight >= viewportWidth) {
+    var timeDateContainerElement = document.querySelector('.clock-container');
+    //timeDateContainerElement.style.bottom = "50%";
+    timeDateContainerElement.style.left = "50%";
+    timeDateContainerElement.style.transform = 'translate(-50%, -0%)';
+    //timeDateContainerElement.style.justifyContent = "center";
+    //timeDateContainerElement.style.alignItems = "center";
+
+    var textTimeElement = document.getElementById("time");
+    textTimeElement.style.fontSize = "7vw";
+    textTimeElement.style.textAlign = "center";
+    var textDateElement = document.getElementById("date");
+    textDateElement.style.fontSize = "4vw";
+    textDateElement.style.textAlign = "center";
+  }
+}
+
 function loadRandomVideo() {
-  var videoBackground = document.getElementById("video-background");
-  var videoFolder = "./background_video/";
-  var videoFiles = [
-    "3037654853.mp4",
-    "3049184066.mp4",
-    "3231081497.mp4",
-    "3238678030.mp4",
-    "3242897185.mp4",
-    "3265233571.mp4"
-    // Add more video as needed
-  ];
+  var viewportWidth = window.innerWidth || document.documentElement.clientWidth;
+  var viewportHeight = window.innerHeight || document.documentElement.clientHeight;
+
+  if (viewportWidth > viewportHeight) {
+    var videoBackground = document.getElementById("video-background");
+    var videoFolder = "./background_video/";
+    var videoFiles = [
+      "3037654853.mp4",
+      "3049184066.mp4",
+      "3231081497.mp4",
+      "3238678030.mp4",
+      "3242897185.mp4",
+      "3252186628.mp4",
+      "3265233571.mp4",
+      "3269475921.mp4",
+      "3271047302.mp4",
+      "3271523338.mp4",
+      "3272404410.mp4",
+      "3274133316.mp4"
+      // Add more video as needed
+    ];
+  } else {
+    var videoBackground = document.getElementById("video-background");
+    var videoFolder = "./background_video_portrait/";
+    var videoFiles = [
+      "2730071537.mp4"
+      // Add more video as needed
+    ];
+  }
+
   var randomIndex = Math.floor(Math.random() * videoFiles.length);
   var randomVideoFiles = videoFiles[randomIndex];
 
@@ -47,3 +117,4 @@ function loadRandomVideo() {
 setInterval(updateTime, 1000);
 setInterval(updateDate, 1000);
 loadRandomVideo();
+adjustTextSize();
